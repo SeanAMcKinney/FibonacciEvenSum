8391a6d811ed4e556eaf0e9c594321eba2482ba1
Sean McKinney <seanamckinney8@gmail.com>
2022-04-12 16:24:55 -0500

    initial commit.  Calculate the sum of all even numbers in a Fibonacci sequence.  No Idea if i'm right.

--- FibonacciEvenSum/Program.cs
+++ FibonacciEvenSum/Program.cs
@@ -10,6 +10,23 @@ namespace FibonacciEvenSum
     {
         static void Main(string[] args)
         {
-        }
-    }
-}
+            int EvenSum = 0;
+                int x = 0; 
+                int y = 1;
+                int z = 0;           
+            for (int i = 0; i < 4000000; i++)
+            {
+                z = x + y;               
+                x = y;
+                y = z;
+                if (i % 2 == 0)
+                {
+                    EvenSum += i;
+                }
+
+            }
+                Console.WriteLine(EvenSum);
+               
+        }//end main
+    }//end class
+}//end namespace
